b189bc5b315299ef46396473c04f48372506d36c
Jessy <jessy-walker@hotmail.com>
2022-12-01 18:48:14 -0500

    Removed MOTD.

--- lua/autorun/3d_particle_system_particles.lua
+++ lua/autorun/3d_particle_system_particles.lua
@@ -1,20 +1,2 @@
-
--- MOTD --
-if (SERVER) then
-
-	-- Do nothing if the server was informed of the new addon.
-	if (file.Exists("3d_particle_system_editor", "DATA")) then return; end
-
-	-- Setup for people who do not have the new editor addon installed.
-	file.CreateDir("3d_particle_system_editor/");
-
-	-- Send out message to everyone.
-	for k,v in pairs(player.GetAll()) do
-		local message = "Update: A new addon was released to aid in the creation of 3D Particles.";
-		v:PrintMessage(HUD_PRINTCENTER, message);
-		v:PrintMessage(HUD_PRINTTALK, message .. " Link: https://steamcommunity.com/sharedfiles/filedetails/?id=2895831461")
-	end
-end
-
 game.Add3DParticles("particles/3d_blood.lua");
 game.Add3DParticles("particles/fusion_explosion.lua");
